2a50f55f0b53422c09d42465e93b48429b764938
Will Luft <william.d.luft@gmail.com>
2020-02-16 14:56:36 -0500

    Added date to show for each post

--- imports/ui/Post.js
+++ imports/ui/Post.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Posts } from '../api/posts';
 
 const Post = ({ post }) => {
-  const { title, body, _id } = post;
+  const { title, body, _id, createdAt } = post;
+
+  const dateAsString = createdAt.toString();
 
   const deletePost = () => {
     Posts.remove(_id);
@@ -10,6 +12,7 @@ const Post = ({ post }) => {
   return (
     <div>
       <h1>{title}</h1>
+      <p>{dateAsString}</p>
       <p>{body}</p>
       <button type='button' onClick={deletePost}>
         Remove Post
